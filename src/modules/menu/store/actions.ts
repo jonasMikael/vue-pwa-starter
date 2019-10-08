22884fb1681dcb43c2bb040ece955f05db179c8c
jonas mikael ferreira da silva <jonas.mikael@outlook.com>
2019-10-08 00:40:44 -0300

    Creation folders and files in module.

---
+++ src/modules/menu/store/actions.ts
@@ -0,0 +1,12 @@
+import { RootState } from '@/store';
+import { ActionContext, ActionTree } from 'vuex';
+import { StateMenu } from './state';
+
+
+type action = (context: ActionContext<StateMenu, RootState>, payload: any) => void
+
+export interface Actions extends ActionTree<StateMenu, RootState> {
+}
+
+export const actions: Actions = {
+};
